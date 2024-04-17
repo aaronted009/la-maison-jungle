f70ac207584ffb922b30e23fd5b4183d0374318c
Aaron <aaronpromail@gmail.com>
2024-04-17 17:03:54 +0100

    added Banner & Cart components

---
+++ src/components/Cart.js
@@ -0,0 +1,23 @@
+function Cart() {
+  const Monstera = { name: "Monstera", price: 8 };
+  const Lierre = { name: "Lierre", price: 10 };
+  const Bouquet = { name: "Bouquet", price: 15 };
+  return (
+    <div>
+      <ul>
+        <li>
+          {Monstera.name} : {Monstera.price}€
+        </li>
+        <li>
+          {Lierre.name} : {Lierre.price}€
+        </li>
+        <li>
+          {Bouquet.name} : {Bouquet.price}€
+        </li>
+      </ul>
+      <div>Total : {Monstera.price + Lierre.price + Bouquet.price}€ </div>
+    </div>
+  );
+}
+
+export default Cart;
