c8669adddf0e30e7ab938dfaf824c24f9aa0ca36
Aaron <aaronpromail@gmail.com>
2024-07-10 23:10:53 +0100

    Add ShoppingList component & conditional display

--- src/data/plantList.js
+++ src/data/plantList.js
@@ -2,46 +2,55 @@ export const plantList = [
 	{
 		name: 'monstera',
 		category: 'classique',
+        isSpecialOffer: true,
 		id: '1ed'
 	},
 	{
 		name: 'ficus lyrata',
 		category: 'classique',
+        isSpecialOffer: false,
 		id: '2ab'
 	},
 	{
 		name: 'pothos argenté',
 		category: 'classique',
+        isSpecialOffer: true,
 		id: '3sd'
 	},
 	{
 		name: 'yucca',
 		category: 'classique',
+        isSpecialOffer: true,
 		id: '4kk'
 	},
 	{
 		name: 'olivier',
 		category: 'extérieur',
+        isSpecialOffer: false,
 		id: '5pl'
 	},
 	{
 		name: 'géranium',
 		category: 'extérieur',
+        isSpecialOffer: true,
 		id: '6uo'
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
+        isSpecialOffer: false,
 		id: '7ie'
 	},
 	{
 		name: 'aloe',
 		category: 'plante grasse',
+        isSpecialOffer: false,
 		id: '8fp'
 	},
 	{
 		name: 'succulente',
 		category: 'plante grasse',
+        isSpecialOffer: true,
 		id: '9vn'
 	}
 ]
