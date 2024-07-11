216290738642c16ff78994612ba9ac9c7bcaf10c
Aaron <aaronpromail@gmail.com>
2024-07-11 12:42:16 +0100

    Add props

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,5 +1,6 @@
 import { plantList } from "../data/plantList";
 import "../style/ShoppingList.css"
+import CareScale from "./CareScale";
 
 function ShoppingList() {
   const categories = plantList.reduce((unique, plant) => {
@@ -23,6 +24,8 @@ function ShoppingList() {
           <li key={plant.id} className="lmj-plant-item">
             {plant.name}
             {plant.isSpecialOffer && <div className="lmj-sales">Soldes</div>}
+            <CareScale careType='water' scaleValue={plant.water} />
+            <CareScale careType='light' scaleValue={plant.light} />
           </li>
         ))}
       </ul>
--- src/data/plantList.js
+++ src/data/plantList.js
@@ -3,54 +3,72 @@ export const plantList = [
 		name: 'monstera',
 		category: 'classique',
         isSpecialOffer: true,
+		light: 2,
+		water: 3,
 		id: '1ed'
 	},
 	{
 		name: 'ficus lyrata',
 		category: 'classique',
         isSpecialOffer: false,
+		light: 3,
+		water: 1,
 		id: '2ab'
 	},
 	{
 		name: 'pothos argenté',
 		category: 'classique',
         isSpecialOffer: true,
+		light: 2,
+		water: 2,
 		id: '3sd'
 	},
 	{
 		name: 'yucca',
 		category: 'classique',
         isSpecialOffer: true,
+		light: 3,
+		water: 3,
 		id: '4kk'
 	},
 	{
 		name: 'olivier',
 		category: 'extérieur',
         isSpecialOffer: false,
+		light: 2,
+		water: 1,
 		id: '5pl'
 	},
 	{
 		name: 'géranium',
 		category: 'extérieur',
         isSpecialOffer: true,
+		light: 3,
+		water: 2,
 		id: '6uo'
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
         isSpecialOffer: false,
+		light: 2,
+		water: 3,
 		id: '7ie'
 	},
 	{
 		name: 'aloe',
 		category: 'plante grasse',
         isSpecialOffer: false,
+		light: 3,
+		water: 3,
 		id: '8fp'
 	},
 	{
 		name: 'succulente',
 		category: 'plante grasse',
         isSpecialOffer: true,
+		light: 1,
+		water: 3,
 		id: '9vn'
 	}
 ]
