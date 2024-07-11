03dd275fdf7579048c11760c9244dd5be81709eb
Aaron <aaronpromail@gmail.com>
2024-07-11 20:02:44 +0100

    Add Footer component and form control to email input field

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import logo from "../images/logo.svg";
 import "../style/App.css";
 import Banner from "./Banner";
-import Cart from "./Cart";
+// import Cart from "./Cart";
 import ShoppingList from "./ShoppingList";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div>
-      <Banner /> <Cart /> <ShoppingList />
+      <Banner /> 
+      {/* <Cart />  */}
+      <ShoppingList />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "../style/Footer.css";
+
+function Footer() {
+  const [inputValue, setInputValue] = useState("Veuillez entrer votre mail");
+  function checkValue(value) {
+    if (!value.includes("@")) {
+      alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide.");
+    } else {
+      setInputValue(value);
+    }
+  }
+  return (
+    <footer className="lmj-footer">
+      <div className="lmj-footer-elem">
+        Pour les passionné·e·s de plantes 🌿🌱🌵
+      </div>
+      <div className="lmj-footer-elem">
+        Laissez-nous votre mail :
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onBlur={(e) => checkValue(e.target.value)}
+        ></input>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
